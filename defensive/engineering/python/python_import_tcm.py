375ac35e5736a86ee6430abc84a5d5d56fd8f4c8
@offsecvault <90124592+offsecvault@users.noreply.github.com>
2022-07-20 17:07:48 -0600

    Add files via upload

---
+++ defensive/engineering/python/python_import_tcm.py
@@ -0,0 +1,10 @@
+
+#!/bin/python3
+
+import sys
+from datetime import datetime as dt # you can import some features from one specific module
+
+print(sys.version)
+# print(datetime.now()) # when it is using an alias it will only understand the alias
+print(dt.now())
+
