fdec0977b20e66388d03f2efbf40f5ad90df3e86
@offsecvault <90124592+offsecvault@users.noreply.github.com>
2022-08-17 07:58:08 -0600

    Create downloadYT_mp3.py

---
+++ tools/downloadYT_mp3.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+#
+# Requirements: pip install pytube
+#
+
+from pytube import Youtube
+import os
+
+song = Youtube(str(input("Enter the URL to download: \n>> ")))
+audio = song.streams.filter(only_audio=true).first()
+print("Enter the destination (leave blank for current folder)")
+destination = str(input(">> ")) or '.'
+download = video.download(output_path=destination)
+base, ext = os.path.splittext(out_file)
+new_file = base + '.mp3'
+os.rename(download, new_file) 
+
+print(song.tittle + "has been successfully downloaded in .mp3 format")
