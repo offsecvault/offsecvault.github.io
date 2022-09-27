f584ba936d3081709ed90fc7bf057bd46a8bcd81
@offsecvault <90124592+offsecvault@users.noreply.github.com>
2022-09-27 09:59:17 -0600

    Update sqlconn_queryexec.php

--- offensive/eWPT/sqlconn_queryexec.php
+++ offensive/eWPT/sqlconn_queryexec.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbhostname='8.8.8.8';
-$dbuser = 'username';
-$dbpassword = 'password';
-$dbname = 'database';
+$dbhostname='8.8.8.8'; #ip address db server
+$dbuser = 'username'; #username
+$dbpassword = 'password'; #password
+$dbname = 'database'; #database name
 
 $connection = mysqli_connect($$dbhostname, $dbuser, $dbpassword, $dbname);
-$query = "SELECT Name, Description FROM Products WHERE ID='2' UNION SELECT Username, Password FROM Accounts;";
+$query = "SELECT Parameter1, Parameter2 FROM table_name WHERE condition UNION SELECT Parameter1, Parameter2 FROM table_name;";
 
 $results = mysqli_query($connection, $query);
 display_results($results);
