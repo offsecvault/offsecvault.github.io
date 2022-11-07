47d2313c613c2e4ea31961cac12933d774346b87
@offsecvault <90124592+offsecvault@users.noreply.github.com>
2022-11-07 14:45:14 -0600

    Update cdn_make_api_call.sh

--- defensive/engineering/bash/cdn_make_api_call.sh
+++ defensive/engineering/bash/cdn_make_api_call.sh
@@ -4,29 +4,37 @@
 #>>>>> comment/uncomment each time you want to collect different information
 #>>>>> create more title section and add the API_URL needed
 
-#TITLE:  
-
-API_URL="type the URL..." 
-
-#defining user auth
+#..... DEFINING_USER_AUTH:
 
 USER=""
 API_KEY=""
 
-#creating the date in RFC 1123 format as part of the steps to generate the password
+#..... LIST_OF_API_URLS_TO_CALL:
 
-DATE='date -u "+%a, %d %b %Y %H:%M:%S %Z"'
+#TITLE:  
+
+API_URL="type the URL..." 
+
+#..... DATE_IN_RFC_1123_FORMAT_AS_PART_OF_THE_STEP_ TO_GENERATE_THE_PASSWORD:
+#>>>>> NOTE: format required is the following: [Thu, 10 Oct 2013 09:12:20 GMT]
+
+DATE=`env LANG="en_US.UTF-8" date -u "+%a, %d %b %Y %H:%M:%S GMT"`
 
 #generating the password needed to make the api_call
 
 PASSWORD=$(echo -n "$DATE" | openssl dgst -sha1 -hmac "$API_KEY" -binary | base64)
-echo " "
 
-#creating the api_call syntax using curl
+#..... CREATING_THE_API-CALL_SYNTAX_USING_CURL:
 
-API_CALL="curl -i --url $API_URL -X GET -u $USER:$PASSWORD -H 'Date: $DATE' -H 'Accept: application/json'"
+#TITLE: CONFIG
+API_CALL="curl -i --url $API_URL_CONFIG \
+-X GET \
+-u $USER:$PASSWORD \
+-H 'x-cnc-date: $DATE' \
+-H 'Accept: application/json' \
+-H 'x-time-zone:GMT+08:00'"
 
-#performing the api_call
+#..... PERFORMING_THE_API-CALL:
 
 eval $API_CALL
 echo " "
