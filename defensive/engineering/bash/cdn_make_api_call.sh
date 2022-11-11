8bda862f69461532d9ad5d26939d41e0dc228ba9
@offsecvault <90124592+offsecvault@users.noreply.github.com>
2022-11-11 11:19:29 -0600

    Update cdn_make_api_call.sh

--- defensive/engineering/bash/cdn_make_api_call.sh
+++ defensive/engineering/bash/cdn_make_api_call.sh
@@ -2,18 +2,11 @@
 
 #>>>>> defining the URL of the cdnetwork API call to make
 #>>>>> comment/uncomment each time you want to collect different information
-#>>>>> create more title section and add the API_URL needed
 
 #..... DEFINING_USER_AUTH:
 
 USER=""
 API_KEY=""
-
-#..... LIST_OF_API_URLS_TO_CALL:
-
-#TITLE:  
-
-API_URL="type the URL..." 
 
 #..... DATE_IN_RFC_1123_FORMAT_AS_PART_OF_THE_STEP_ TO_GENERATE_THE_PASSWORD:
 #>>>>> NOTE: format required is the following: [Thu, 10 Oct 2013 09:12:20 GMT]
@@ -24,9 +17,12 @@ DATE=`env LANG="en_US.UTF-8" date -u "+%a, %d %b %Y %H:%M:%S GMT"`
 
 PASSWORD=$(echo -n "$DATE" | openssl dgst -sha1 -hmac "$API_KEY" -binary | base64)
 
-#..... CREATING_THE_API-CALL_SYNTAX_USING_CURL:
+#..... LIST_OF_API_URLS_TO_CALL:
 
-#TITLE: CONFIG
+#TITLE: TESTING
+
+API_URL="type the URL..." 
+
 API_CALL="curl -i --url $API_URL_CONFIG \
 -X GET \
 -u $USER:$PASSWORD \
